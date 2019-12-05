4f20c98cc6dce9762931b5552e3ebd4b65e8fab4
tobizaru <tinpiragomi@outlook.jp>
2019-12-05 23:10:00 +0900

    1st

---
+++ embed_text/embed_text.rb
@@ -0,0 +1,20 @@
+require "prawn"
+
+text = "相原　久美子"
+xys = [
+  [157, 333],
+]
+
+Prawn::Document.generate("transparent_text.pdf",
+                          :page_size => 'A4',
+                          :page_layout => :landscape) do |pdf|
+
+  pdf.font "Koruri-Regular.ttf", :size => 11
+  pdf.transparent(0) do
+  # pdf.transparent(1) do
+    xys.each{|xy|
+      pdf.text_box text, :at=>xy
+    }
+  end
+  system('pdftk '+ARGV[0]+' multistamp transparent_text.pdf output '+ARGV[1])
+end
