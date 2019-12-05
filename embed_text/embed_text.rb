626bc25f734a5777fb0278bee31867e39505bd91
tobizaru <tinpiragomi@outlook.jp>
2019-12-05 23:42:04 +0900

    fixed another bug

--- embed_text/embed_text.rb
+++ embed_text/embed_text.rb
@@ -2,7 +2,25 @@ require "prawn"
 
 text = "相原　久美子"
 xys = [
+  [
   [157, 333],
+  ], # page1
+  [], #page2
+  [], #page3
+  [], #page4
+  [], #page5
+  [], #page6
+  [], #page7
+  [], #page8
+  [], #page9
+  [], #page10
+  [], #page11
+  [], #page12
+  [], #page13
+  [], #page14
+  [], #page15
+  [],#page16
+  [], #page17
 ]
 
 Prawn::Document.generate("transparent_text.pdf",
@@ -12,9 +30,14 @@ Prawn::Document.generate("transparent_text.pdf",
   pdf.font "Koruri-Regular.ttf", :size => 11
   pdf.transparent(0) do
   # pdf.transparent(1) do
-    xys.each{|xy|
+  xys.each_with_index{|page, p|
+    page.each{|xy|
       pdf.text_box text, :at=>xy
     }
-  end
+    if xys.length - 1  != p then
+      pdf.start_new_page
+    end
+  }
+end
 end
 system('pdftk '+ARGV[0]+' multistamp transparent_text.pdf output '+ARGV[1])
